34ca245c7f4621ef493b73d282ab21be78c50c5c
Horacio Mota <horaciomota93@gmail.com>
2023-10-02 18:15:50 +0100

    First Commit

---
+++ PokeSwift_V3/PokeSwift_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  PokeSwift_V3App.swift
+//  PokeSwift_V3
+//
+//  Created by Horacio Mota on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeSwift_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
