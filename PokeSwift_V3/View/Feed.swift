3ab9179f7c40d3565e6543001e8ce55a56d79b4e
Horacio Mota <horaciomota93@gmail.com>
2023-10-03 20:38:34 +0100

    PokemonCard() added on Feed view

--- PokeSwift_V3/View/Feed.swift
+++ PokeSwift_V3/View/Feed.swift
@@ -46,6 +46,8 @@ struct Feed: View {
                         
                     }
                     
+                    PokemonCard()
+                    
                     Spacer()
                     
                 }
